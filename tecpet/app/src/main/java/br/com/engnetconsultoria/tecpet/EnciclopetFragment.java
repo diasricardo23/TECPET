d8cf0c5bcbda01161a8aae599df1ae4b092c9b5c
diasricardo23 <diasmacedo.ricardo@gmail.com>
2017-04-21 05:36:00 -0300

    Add files via upload

---
+++ tecpet/app/src/main/java/br/com/engnetconsultoria/tecpet/EnciclopetFragment.java
@@ -0,0 +1,29 @@
+package com.example.ricardo.tecpet;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EnciclopetFragment extends Fragment {
+
+
+    public EnciclopetFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.enciclopet_fragment, container, false);
+    }
+
+}
